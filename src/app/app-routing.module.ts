af9ba406abe08b11284ade5823c42bf9223ab723
noelm <noelmillan@securekids.es>
2022-04-29 00:43:27 +0200

    arreglado un fallo del footer y teclado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,10 +20,7 @@ const routes: Routes = [
   },
   {
     path: 'login',
-    loadChildren: () => import('./pages/login/login.module')
-                            .then( m => m.LoginPageModule),
-                            canActivate: [AuthGuard],
-                            data: { authGuardPipe: redirectLoggedInToList }
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule),
   },
   {
     path: 'register',
